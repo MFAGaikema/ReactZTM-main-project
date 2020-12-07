c25e04aa76c97e4a0f2013cee1815107eb435599
Marijn Gaikema <marijngaikema@gmail.com>
2020-12-07 17:27:46 +0100

    broken up homepage into components and styling the menu items

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './../../css/homepage.styles.css'
+import Directory from "./../../components/directory/directory.component"
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  )
+}
+
+export default Homepage;
+
